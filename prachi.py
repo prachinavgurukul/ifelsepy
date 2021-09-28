c4018cb6fe31bf59defd9009ed61bbcabd674f25
prachinavgurukul <91555689+prachinavgurukul@users.noreply.github.com>
2021-09-28 21:25:21 +0530

    Add files via upload

---
+++ prachi.py
@@ -0,0 +1,33 @@
+# price_milk="1"
+# print("10L milk "+ price_milk*10+"rupees ka aata hai");
+# year=2016
+# name="navgurukul"
+# print(name+"2016"+"mein suru hua tha"
+
+
+# mangoes=5
+# mangoes2=mangoes+5
+# mangoes3=mangoes2/5
+# print(mangoes2)
+# print(mangoes3)
+# day=str(input("day enter kro"))
+# if day=="monday":
+#     print("Rajma Chawal")
+# elif day=="tuesday":
+#     print("Pav Bhaji")
+# elif day=="sunday":
+#     print("Pulav")
+# else:
+#     print("poha")
+salary=int(input("enter the payment"))
+service=int(input("enter year"))
+bonus_amt=salary*5/100
+if service>5:
+    print("bonus_amt")
+else:
+    print("salary")
+    
+
+
+
+
